4c5b8fcdabcf258aa50ea15274eb54428af77f2a
Marsellus47 <mjackow@gmail.com>
2018-09-20 18:06:00 +0200

    Interface

---
+++ Fake.DataAccess/Interfaces/Random/IRandomProvider.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fake.DataAccess.Interfaces.Random
+{
+    public interface IRandomProvider
+    {
+        long Number(long min = long.MinValue, long max = long.MaxValue);
+        IEnumerable<long> Numbers(int count, long min = long.MinValue, long max = long.MaxValue);
+
+        byte Digit(byte min = byte.MinValue, byte max = byte.MaxValue);
+        IEnumerable<byte> Digits(int count, byte min = byte.MinValue, byte max = byte.MaxValue);
+
+        long Even(long min = long.MinValue, long max = long.MaxValue);
+        IEnumerable<long> Even(int count, long min = long.MinValue, long max = long.MaxValue);
+
+        long Odd(long min = long.MinValue, long max = long.MaxValue);
+        IEnumerable<long> Odd(int count, long min = long.MinValue, long max = long.MaxValue);
+
+        double Decimal(double min = double.MinValue, double max = double.MaxValue);
+        IEnumerable<double> Decimals(int count, double min = double.MinValue, double max = double.MaxValue);
+
+        char Char(char min = char.MinValue, char max = char.MaxValue);
+        IEnumerable<char> Chars(int count, char min = char.MinValue, char max = char.MaxValue);
+
+        string String(int minLength = 1, int maxLength = int.MaxValue, char minChar = char.MinValue, char maxChar = char.MaxValue);
+        IEnumerable<string> Strings(int count, int minLength = 1, int maxLength = int.MaxValue, char minChar = char.MinValue, char maxChar = char.MaxValue);
+
+        string Hash(int length = 40, bool upperCase = false);
+        IEnumerable<string> Hashes(int count, int length = 40, bool upperCase = false);
+
+        bool Boolean();
+        bool Boolean(float weight = 0);
+        IEnumerable<bool> Booleans(int count);
+        IEnumerable<bool> Booleans(int count, float weight = 0);
+
+        T EnumerationElement<T>(IEnumerable<T> enumeration);
+        IEnumerable<T> EnumerationElements<T>(int count, IEnumerable<T> enumeration);
+
+        IEnumerable<T> Shuffle<T>(IEnumerable<T> enumerable);
+
+        string Word();
+        IEnumerable<string> Words(int minCount = int.MinValue, int maxCount = int.MaxValue);
+
+        Guid Uuid();
+        IEnumerable<Guid> Uuids(int count);
+
+        string Locale();
+        IEnumerable<string> Locales(int count);
+
+        string AlphaNumeric(short minLength = 1, short maxLength = short.MaxValue);
+        IEnumerable<string> AlphaNumerics(int count, short minLength = 1, short maxLength = short.MaxValue);
+
+        string Hexadecimal(int length = 1);
+        IEnumerable<string> Hexadecimals(int count, short minLength = 1, short maxLength = short.MaxValue);
+
+        T Weighted<T>(IEnumerable<T> items, float[] weights);
+    }
+}
